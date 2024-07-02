35fc2e349b7a1a62175376d299cfc374ebdb966e
danyabravo <danilk607@gmail.com>
2024-07-02 19:35:12 +0300

    solve task/007

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val KEY_MESSAGE = "messageKey"
+
+
+    lateinit var titleTextView: TextView
+    lateinit var changeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        changeButton = findViewById(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            titleTextView.text = "I am an Android Developer!"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(KEY_MESSAGE, titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            titleTextView.text = savedInstanceState.getString(KEY_MESSAGE)
+        }
     }
 }
